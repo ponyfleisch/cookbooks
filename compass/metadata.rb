0acc97618120f6dad06744d8d7acc1e487acf20d
Claudio Mettler <github@ponyfleisch.ch>
2013-10-07 19:48:10 +0800

    initial commit

---
+++ compass/metadata.rb
@@ -0,0 +1,7 @@
+name             'compass'
+maintainer       'Claudio Mettler'
+maintainer_email 'code@ponyfleisch.ch'
+license          ''
+description      'Installs/Configures compass'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ compass/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: compass
+# Recipe:: default
+#
+# Copyright 2013, Claudio Mettler
+#
+
+gem_package "compass" do
+end
+
