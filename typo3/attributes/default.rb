cdc855e1d09a1f8751c5f334918afe2a621a3f6d
Claudio Mettler <github@ponyfleisch.ch>
2013-10-22 23:12:23 +0800

    fix

--- typo3/attributes/default.rb
+++ typo3/attributes/default.rb
@@ -1,3 +1,3 @@
-default['typo3']['root'] = node['apache']['docroot_dir']
+default['typo3']['root'] = '/var/www/html'
 default['typo3']['version'] = '6.1.5'
 default['typo3']['file'] = "http://prdownloads.sourceforge.net/typo3/typo3_src-#{node['typo3']['version']}.tar.gz?download"
