b545c340cbe65d9d0b9a0e5d838e22e565ed47fb
Claudio Mettler <github@ponyfleisch.ch>
2013-10-22 22:54:49 +0800

    typo3 added

---
+++ typo3/metadata.rb
@@ -0,0 +1,7 @@
+name             'typo3'
+maintainer       'Claudio Mettler'
+maintainer_email 'info@ctrlm.ch'
+license          'MIT'
+description      'Installs/Configures typo3'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ typo3/recipes/default.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: typo3
+# Recipe:: default
+#
+# Copyright 2013, Claudio Mettler
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+remote_file "#{Chef::Config[:file_cache_path]}/typo3-src-#{node['typo3']['version']}.tar.gz" do
+  source node['typo3']['file']
+end
+
+directory "#{node['typo3']['root']}/typo3_src/" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+
+bash 'extract typo3 source' do
+  extract_path = "#{node['typo3']['root']}/typo3_src/typo3_src-#{node['typo3']['version']}"
+  
+  cwd "#{node['typo3']['root']}/typo3_src/"
+  code <<-EOH
+    tar xzf #{Chef::Config[:file_cache_path]}/typo3-src-#{node['typo3']['version']}.tar.gz
+    EOH
+  not_if { ::File.exists?(extract_path) }
+end
